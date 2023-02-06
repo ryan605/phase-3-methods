556f55e26a3adeddf8c32bcc1f932a24adc7ff08
ryan605 <langatryan26@gmail.com>
2023-02-06 20:27:06 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,20 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet(name="Naureen")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    puts num1
+    puts num2
+    num1 + num2
+end
+
+puts add num1(2) + num2(5)
